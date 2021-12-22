c770e044e0d2ea6fbb1c013431b0a257812708db
Jin-s-work <61958748+Jin-s-work@users.noreply.github.com>
2021-12-22 16:59:57 +0900

    12.22 two point

--- 플로이드 와샬/BOJ 11404 플로이드.cpp
+++ 플로이드 와샬/BOJ 11404 플로이드.cpp
@@ -22,33 +22,57 @@ int h, w;
 
 
 int main(){
-    
+
     ios_base::sync_with_stdio(0);
     cin.tie(0);
+
+
+    cin >> n >> m;
     
+    int dist[101][101];
     
-    string s, t;
-    cin >> s >> t;
+    for(int i=1;i<=n;i++){
+        for(int j=1;j<=n;j++){
+            if(i == j)
+                dist[i][j] = 0;
+            else
+                dist[i][j] = MAX;
+        }
+    }       // 같은 경우는 0이고 다른 경우는 바뀔 수 있으므로 MAX로 해줌
     
-    string fs, ft;
-    
-    
-    for(int i=0;i<t.length();i++){
-        fs += s;
+    for(int i=0;i<m;i++){
+        int a, b, c;
+        cin >> a >> b >> c;
+
+        dist[a][b] = min(dist[a][b], c);
+        // dist[a][b] 자체가 값이므로, c와 그 중에 작은 값을 넣어준다.
+        
+        // 0부터 진행되므로 1씩 작게해서 해준다.
     }
-    // t의 횟수만큼 s를 더해줌
-    for(int i=0;i<s.length();i++){
-        ft += t;
+
+    for(int k=1;k<=n;k++){
+        for(int i=1;i<=n;i++){
+            for(int j=1;j<=n;j++){
+                dist[i][j] = min(dist[i][j], dist[i][k] + dist[k][j]);
+            }
+        }
     }
-    // s의 갯수만큼 t에 더해줌
+    // dist로 이어지는 걸 비교하면서 갱신한다.
     
-    if(fs == ft)
-        cout << 1 << '\n';
-    else
-        cout << 0 << '\n';
-    
+    for(int i=1;i<=n;i++){
+        for(int j=1;j<=n;j++){
+            if(dist[i][j] == MAX)
+                cout << 0 << " ";
+            else
+                cout << dist[i][j] << " ";
+        }   // MAX일 경우 0으로 하고 아닐 경우 그 값을 그냥 넣어준다.
+            // MAX면 안바뀐 것임
+        cout << '\n';
+    }
+
+
     return 0;
-    
+
 }
 
 //
