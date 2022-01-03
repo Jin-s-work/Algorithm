5d535b15af95429bc581e84bb868edc30103deed
Jin-s-work <61958748+Jin-s-work@users.noreply.github.com>
2022-01-03 14:41:09 +0900

    1.3 SDS Algorithm Day 1

---
+++ SDS_알고리즘/Day 1/BOJ 3425 고스택.cpp
@@ -0,0 +1,62 @@
+#include <iostream>  // stdio.h 와 같은 것
+#include <vector>
+#include <string>
+#include <algorithm>
+#include <stack>
+#include <queue>
+#include <cstring>
+#include <cmath>
+#include <functional>
+#include <map>
+#include <set>
+
+// control i
+#define MAX 987654321
+#define mod 1000000007
+#define pii pair<int, int>
+using ll = long long;
+using namespace std;
+int n,m,k;
+int l,r,t;
+int h, w;
+
+
+int main(){
+    
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    
+    
+    string s, t;
+    cin >> s >> t;
+    
+    string fs, ft;
+    
+    
+    for(int i=0;i<t.length();i++){
+        fs += s;
+    }
+    // t의 횟수만큼 s를 더해줌
+    for(int i=0;i<s.length();i++){
+        ft += t;
+    }
+    // s의 갯수만큼 t에 더해줌
+    
+    if(fs == ft)
+        cout << 1 << '\n';
+    else
+        cout << 0 << '\n';
+    
+    return 0;
+    
+}
+
+//
+//7
+//1GTW
+//2PDF
+//3REF
+//3RDF
+//2REF
+//1PTF
+//3RES
