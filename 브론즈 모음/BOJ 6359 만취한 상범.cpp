9eca8f25fd425429e94a9205a9d643ade66b2906
Jin-s-work <9114jin@hanmail.net>
2022-03-17 01:38:18 +0900

    3.17 bronze

---
+++ 브론즈 모음/BOJ 6359 만취한 상범.cpp
@@ -0,0 +1,49 @@
+#include <iostream>  // stdio.h 와 같은 것
+#include <vector>
+#include <string>
+#include <algorithm>
+#include <stack>
+#include <queue>
+#include <cstring>
+#include <cmath>
+#include <functional>
+#include <map>
+#include <unordered_map>
+#include <set>
+
+// control i
+#define MAX 987654321
+#define mod 10007
+#define pii pair<int, int>
+using ll = long long;
+using namespace std;
+int n,m,k;
+int l,r,t;
+int h;
+
+
+
+
+
+int main(){
+
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+
+    
+    cin >> t;
+    
+    while(t--){
+        
+        int a;
+        cin >> a;
+        cout << int(sqrt(a)) << '\n';
+        
+    }
+    
+    
+    
+    
+    return 0;
+
+}
