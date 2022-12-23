be44c6be14434730b650e0db98a9c0015def1109
Jin-s-work <9114jin@hanmail.net>
2022-12-23 15:40:04 +0900

    12.23 BackTracking

---
+++ 백트래킹/BOJ 1182 부분수열의 합.py
@@ -0,0 +1,17 @@
+n, s = map(int, input().split())
+arr = list(map(int, input().split()))
+ans = 0
+
+def DFS(num, Sum):
+    global ans
+    if num==n:
+        return
+    if Sum + arr[num] == s:
+        ans += 1
+    
+    DFS(num+1, Sum)
+    DFS(num+1, Sum + arr[num])
+    return
+
+DFS(0,0)
+print(ans)
