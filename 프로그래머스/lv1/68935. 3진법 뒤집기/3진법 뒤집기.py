8c4e707fb1de9f9607f45c6aae0c8261f0b63186
Sangwoo Jin <61958748+Jin-s-work@users.noreply.github.com>
2023-07-17 19:58:34 +0900

    [level 1] Title: 3진법 뒤집기, Time: 0.03 ms, Memory: 10.3 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/68935. 3진법 뒤집기/3진법 뒤집기.py
@@ -0,0 +1,14 @@
+def solution(n):
+    answer = 0
+
+    st = ""
+    while n != 0:
+        st += str(n % 3)
+        n //= 3
+    
+    answer = int(st, 3)
+    
+    
+    
+    
+    return answer
