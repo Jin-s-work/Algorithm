940125c61549c25175f4b410f62fc2f0d9de7f32
Sangwoo Jin <61958748+Jin-s-work@users.noreply.github.com>
2023-08-08 15:12:38 +0900

    [unrated] Title: 두 원 사이의 정수 쌍, Time: 388.00 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/unrated/181187. 두 원 사이의 정수 쌍/두 원 사이의 정수 쌍.py
+++ 프로그래머스/unrated/181187. 두 원 사이의 정수 쌍/두 원 사이의 정수 쌍.py
@@ -5,11 +5,10 @@ def solution(r1, r2):
     
     for i in range(0, r1):
         answer += int(math.sqrt(r2*r2 - i*i)) - int(math.sqrt(r1*r1 - i*i - 1))
+        # x축을 늘리면서 그 x축의 개수를 세어준다.
     for i in range(r1, r2):
         answer += int(math.sqrt(r2*r2 - i*i))
-            
-    
-    
+        # 원과 맞닿는
     
     
     return answer * 4
