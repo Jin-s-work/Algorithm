e409466ad8efba1b032a673c6ed948d327da7097
Sangwoo Jin <61958748+Jin-s-work@users.noreply.github.com>
2023-08-08 15:56:23 +0900

    [level 2] Title: 다음 큰 숫자, Time: 0.00 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/lv2/12911. 다음 큰 숫자/다음 큰 숫자.py
+++ 프로그래머스/lv2/12911. 다음 큰 숫자/다음 큰 숫자.py
@@ -1,7 +1,6 @@
 def solution(n):
     answer = 0
     
-    
     num = bin(n).count("1")
     
     while True:
@@ -12,3 +11,32 @@ def solution(n):
         
     
     return answer
+
+
+
+# def solution(n):
+#     answer = 0
+    
+#     num1 = 0
+#     one = str(bin(n))
+#     for i in one:
+#         if i == '1':
+#             num1+=1
+    
+    
+#     num = n
+#     while num <= 1000000:
+#         num2 = 0
+#         num+=1
+#         two = str(bin(num))
+#         for i in two:
+#             if i == '1':
+#                 num2+=1
+#         if num1 == num2:
+#             return num
+        
+        
+    
+    
+#     return answer
+
