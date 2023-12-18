19aa48e7df9b95453914670b6e0bdb259ee32708
Sangwoo Jin <61958748+Jin-s-work@users.noreply.github.com>
2023-12-18 15:22:45 +0900

    [Silver I] Title: 회의실 배정, Time: 388 ms, Memory: 130204 KB -BaekjoonHub

--- 백준/Silver/1931. 회의실 배정/회의실 배정.py
+++ 백준/Silver/1931. 회의실 배정/회의실 배정.py
@@ -1,19 +1,26 @@
+import sys
+input = sys.stdin.readline
+
 n = int(input())
 arr = []
-for _ in range(n):
-    arr.append(list(map(int, input().split())))
 
-# x[1] 즉 끝나는 시간을 먼저 정렬해줌으로서 끝나는 시간으로 정렬
-arr.sort(key = lambda x : (x[1], x[0]))
+for k in range(n):
+    arr.append(list(map(int, input().split())))
+    # a, b = list(map(int, input().split()))
+    # arr.append([a, b])
+
+arr.sort(key=lambda x : (x[1], x[0]))
+# 두번째를 오름차순한 다음에 첫번째것을 오름차순 정렬
+
 end = 0
 ans = 0
 for i in range(len(arr)):
     if arr[i][0] < end:
-        continue   
-    # end가 arr[i][1] 즉 끝나는 시간보다 작을 경우에는 그냥 넘어감
+        continue
+    # 미리 지정해 둔 끝인 end가 그 다음의 arr[i][0]보다 작을 경우에는
+    # 할 필요가 없으므로 continue로 넘어간다.
     
-    end = arr[i][1]   # 끝나는 시간을 end로 설정
-    ans += 1
-    # 아닐 경우 end를 갱신해주고 답 증가시킴
+    end = arr[i][1]
+    ans += 1 
     
 print(ans)
