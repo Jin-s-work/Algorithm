7cf00c4e07126cb42fc4bcb1002bd1815e1cdb34
Sangwoo Jin <61958748+Jin-s-work@users.noreply.github.com>
2024-03-28 22:50:59 +0900

    [level 2] Title: 가격이 제일 비싼 식품의 정보 출력하기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

---
+++ 프로그래머스/2/131115. 가격이 제일 비싼 식품의 정보 출력하기/가격이 제일 비싼 식품의 정보 출력하기.sql
@@ -0,0 +1,31 @@
+# SELECT *
+# FROM FOOD_PRODUCT
+# ORDER BY PRICE DESC
+# LIMIT 1
+
+# # SELECT *
+# # FROM FOOD_PRODUCT
+# # ORDER BY PRICE DESC
+# # LIMIT 1
+
+
+# # # -- 코드를 입력하세요
+# # # SELECT *
+# # # FROM FOOD_PRODUCT
+# # # ORDER BY PRICE DESC
+# # # LIMIT 1;
+# # # -- 전체에 있어서 PRICE에 대해 내림차순으로 설정하고 LIMIT으로 1개만 뽑늗다.
+
+
+SELECT *
+FROM FOOD_PRODUCT
+ORDER BY PRICE DESC
+LIMIT 1
+
+
+
+
+
+
+
+
