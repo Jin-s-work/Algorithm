d6671ad21d8b503dae7b10aeb3b0b6d1377dd7fa
Sangwoo Jin <61958748+Jin-s-work@users.noreply.github.com>
2024-04-04 14:37:19 +0900

    [level 2] Title: 타겟 넘버, Time: 3.00 ms, Memory: 10.2 MB -BaekjoonHub

---
+++ 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.py
@@ -0,0 +1,32 @@
+
+
+
+
+
+
+
+
+
+def DFS(arr, idx, num, t):
+    
+    cnt = 0
+    
+    if idx == len(arr):
+        if num == t:
+            return 1
+        else:
+            return 0
+    
+    cnt += DFS(arr, idx + 1, num + arr[idx], t)
+    cnt += DFS(arr, idx + 1, num - arr[idx], t)
+    
+    return cnt
+    
+
+
+def solution(numbers, target):
+    answer = 0
+    
+    answer = DFS(numbers, 0, 0, target)
+    
+    return answer
