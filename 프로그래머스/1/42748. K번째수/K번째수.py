15087aeea0bd559cc393de7fc3d93d940b9d3910
Sangwoo Jin <61958748+Jin-s-work@users.noreply.github.com>
2024-04-08 18:09:42 +0900

    [level 1] Title: K번째수, Time: 0.01 ms, Memory: 10.2 MB -BaekjoonHub

---
+++ 프로그래머스/1/42748. K번째수/K번째수.py
@@ -0,0 +1,13 @@
+def solution(array, commands):
+    answer = []
+    
+    for i in range(len(commands)):
+        arr = []
+        for j in range(commands[i][0]-1, commands[i][1]):
+            arr.append(array[j])
+        arr.sort()
+        answer.append(arr[commands[i][2]-1])
+    
+    
+    
+    return answer
