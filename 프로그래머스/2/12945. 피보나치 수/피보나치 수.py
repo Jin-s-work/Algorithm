e896fcdeddee4d2cebf9d3ed7a4ccd7930920c6a
Sangwoo Jin <61958748+Jin-s-work@users.noreply.github.com>
2024-04-16 15:23:10 +0900

    [level 2] Title: 피보나치 수, Time: 21.04 ms, Memory: 13.6 MB -BaekjoonHub

---
+++ 프로그래머스/2/12945. 피보나치 수/피보나치 수.py
@@ -0,0 +1,13 @@
+def solution(n):
+    answer = 0
+    
+    dp = [0] * (n+1)
+    
+    dp[0] = 0
+    dp[1] = 1
+    for i in range(0, n-1):
+        dp[i+2] = (dp[i] + dp[i+1]) % 1234567
+    
+    
+    
+    return dp[n] % 1234567
