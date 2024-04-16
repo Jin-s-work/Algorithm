cd96c8da329b93ba981dbd41748066dcfdbcaf7f
Sangwoo Jin <61958748+Jin-s-work@users.noreply.github.com>
2024-04-16 15:17:17 +0900

    [level 2] Title: 숫자의 표현, Time: 5.26 ms, Memory: 10.1 MB -BaekjoonHub

---
+++ 프로그래머스/2/12924. 숫자의 표현/숫자의 표현.py
@@ -0,0 +1,15 @@
+def solution(n):
+    answer = 0
+    
+    for i in range(1, n+1):
+        sum = 0
+        for j in range(i, n+1):
+            sum += j
+            if sum > n:
+                break
+            elif sum == n:
+                answer += 1
+                break
+    
+    
+    return answer
