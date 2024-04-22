23bb17d812b472fbf121b4b1e895480c7a905539
Sangwoo Jin <61958748+Jin-s-work@users.noreply.github.com>
2024-04-22 15:48:32 +0900

    [level 0] Title: 숫자 비교하기, Time: 0.01 ms, Memory: 16.4 MB -BaekjoonHub

---
+++ 프로그래머스/0/120807. 숫자 비교하기/숫자 비교하기.swift
@@ -0,0 +1,14 @@
+import Foundation
+
+func solution(_ num1:Int, _ num2:Int) -> Int {
+    
+    
+    if num1 == num2{
+        return 1
+    }
+    else{
+        return -1
+    }
+    
+    return 0
+}
