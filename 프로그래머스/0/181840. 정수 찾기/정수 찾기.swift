6fbbf88091fbddba39fbc0230ceb97b5edead812
Sangwoo Jin <61958748+Jin-s-work@users.noreply.github.com>
2024-04-24 01:37:37 +0900

    [level 0] Title: 정수 찾기, Time: 0.03 ms, Memory: 16.4 MB -BaekjoonHub

---
+++ 프로그래머스/0/181840. 정수 찾기/정수 찾기.swift
@@ -0,0 +1,13 @@
+import Foundation
+
+func solution(_ num_list:[Int], _ n:Int) -> Int {
+    
+    if num_list.contains(n){
+        return 1
+    }
+    else{
+        return 0
+    }
+    
+    return 0
+}
