07a0c69688a4a19c711a6eb750ac1a081f54ee39
Sangwoo Jin <61958748+Jin-s-work@users.noreply.github.com>
2024-04-24 01:22:54 +0900

    [level 0] Title: 문자열 곱하기, Time: 0.04 ms, Memory: 16.2 MB -BaekjoonHub

---
+++ 프로그래머스/0/181940. 문자열 곱하기/문자열 곱하기.swift
@@ -0,0 +1,11 @@
+import Foundation
+
+func solution(_ my_string:String, _ k:Int) -> String {
+    
+    var ans = ""
+    for i in 0..<k{
+        ans.append(my_string)
+    }
+    
+    return ans
+}
