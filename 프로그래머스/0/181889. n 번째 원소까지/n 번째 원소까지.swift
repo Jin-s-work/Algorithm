5abea09172c80379ed552841c71073eb47b6fc72
Sangwoo Jin <61958748+Jin-s-work@users.noreply.github.com>
2024-04-23 19:34:57 +0900

    [level 0] Title: n 번째 원소까지, Time: 0.05 ms, Memory: 16.4 MB -BaekjoonHub

---
+++ 프로그래머스/0/181889. n 번째 원소까지/n 번째 원소까지.swift
@@ -0,0 +1,14 @@
+import Foundation
+
+func solution(_ num_list:[Int], _ n:Int) -> [Int] {
+    
+    var ans : [Int] = []
+    
+    for (i, num) in num_list.enumerated(){
+        if i < n{
+            ans.append(num)
+        }
+    }
+    
+    return ans
+}
