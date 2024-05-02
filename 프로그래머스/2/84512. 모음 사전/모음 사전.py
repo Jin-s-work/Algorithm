d038666e1183fce2939ee1877b45326610407f76
Sangwoo Jin <61958748+Jin-s-work@users.noreply.github.com>
2024-05-03 01:33:37 +0900

    [level 2] Title: 모음 사전, Time: 1.46 ms, Memory: 10.6 MB -BaekjoonHub

--- 프로그래머스/2/84512. 모음 사전/모음 사전.py
+++ 프로그래머스/2/84512. 모음 사전/모음 사전.py
@@ -1,19 +1,22 @@
 def solution(word):
     answer = 0
     
-    # 5진법?
     arr = []
     words = "AEIOU"
     
     def DFS(num, k):
         if num == 5:
             return
+        
         for i in range(len(words)):
             arr.append(k + words[i])
             DFS(num + 1, k + words[i])
+        # 처음에 A 넣고, AA, AAA, AAAA, AAAAA
+        # AAAAA에서 돌아와서 그 다음인 E로 넣는다. 따라서 AAAAE, 5개이므로 끝나니까 다음인 I
+        # AAAAI,,,, AAAAO, AAAAU, 다음은 AAAE
     
     DFS(0, "")
     answer = arr.index(word) + 1
-    # arr에서 word의 위치를 찾아서 1을 더해주면 된다.
+    
     
     return answer
