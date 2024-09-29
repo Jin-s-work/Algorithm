4abbd581d476766985883b056d0d83e4b9b27d71
Sangwoo Jin <61958748+Jin-s-work@users.noreply.github.com>
2024-09-29 16:00:30 +0900

    [level 3] Title: 네트워크, Time: 0.36 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/3/43162. 네트워크/네트워크.py
+++ 프로그래머스/3/43162. 네트워크/네트워크.py
@@ -1,39 +1,17 @@
-# def solution(n, computers):
-#     answer = 0
-    
-#     def DFS(now):
-#         check[now] = 1
-#         for next in range(n):
-#             if not check[next] and computers[now][next]:
-#                 DFS(next)
-    
-#     check = [0] * len(computers)
-#     for i in range(n):
-#         if not check[i]:
-#             DFS(i)
-#             answer += 1
-    
-#     return answer
-
-from collections import deque
 def solution(n, computers):
     answer = 0
     
-    def BFS(computers, check, now):
-        q = deque([now])
+    def DFS(now):
         check[now] = 1
+        for next in range(n):
+            if not check[next] and computers[now][next]:
+                DFS(next)
     
-        while q:
-            v = q.popleft()
-            for i in range(n):
-                if not check[i] and computers[v][i]:
-                    q.append(i)
-                    check[i] = 1
-            
-    check = [0] * (len(computers) + 1)
+    
+    check = [0] * len(computers)
     for i in range(n):
         if not check[i]:
-            BFS(computers, check, i)
+            DFS(i)
             answer += 1
     
     
