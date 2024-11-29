d765f7bbabff30655f635e15cadd37c5c36b0e6c
Sangwoo Jin <61958748+Jin-s-work@users.noreply.github.com>
2024-11-29 17:10:53 +0900

    [level 2] Title: 피로도, Time: 55.17 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/2/87946. 피로도/피로도.py
+++ 프로그래머스/2/87946. 피로도/피로도.py
@@ -1,22 +1,19 @@
 def solution(k, dungeons):
-
+    
     global answer
     answer = 0
     
-    check = [0] * len(dungeons)
-    
+    check = [0] * (len(dungeons) + 1)
     def DFS(k, num, dungeons):
         global answer
-        if answer < num:
-            answer = num
-            
+        answer = max(answer, num)
+        
         for i in range(len(dungeons)):
             if k >= dungeons[i][0] and not check[i]:
-                check[i] = 1
+                check[i] = True
                 DFS(k - dungeons[i][1], num + 1, dungeons)
-                check[i] = 0
+                check[i] = False
         
-    
     DFS(k, 0, dungeons)
     
     
