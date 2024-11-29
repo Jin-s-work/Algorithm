9365a9f3eb33f819b0bb313f8a68382a84f6c4b0
Sangwoo Jin <61958748+Jin-s-work@users.noreply.github.com>
2024-11-29 18:03:26 +0900

    [level 2] Title: 전화번호 목록, Time: 456.48 ms, Memory: 34.6 MB -BaekjoonHub

--- 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.py
+++ 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.py
@@ -1,17 +1,17 @@
 def solution(phone_book):
     answer = True
     
-    hash_map = {}
-    for phone_number in phone_book:
-        hash_map[phone_number] = 1
+    dic = {}
+    for k in phone_book:
+        dic[k] = 1
     
-    for phone_number in phone_book:
-        tmp = ""
-        for number in phone_number:
-            tmp += number
-            if tmp in hash_map and tmp != phone_number:
+    for phone in phone_book:
+        st = ""
+        for num in phone:
+            st += num
+            if st in dic and st != phone:
                 answer = False
-            # 더해가며 만든 tmp가 hash map에 있는데 완전 똑같은 경우가 아닐 경우에 False
     
-     
+    
+    
     return answer
