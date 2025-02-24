1aa7b3b0df0f7def8abf4f3d88efc2d5d653c1aa
Sangwoo Jin <61958748+Jin-s-work@users.noreply.github.com>
2025-02-24 17:10:18 +0900

    [Silver III] Title: N과 M (2), Time: 88 ms, Memory: 108384 KB -BaekjoonHub

--- 백준/Silver/15650. N과 M （2）/N과 M （2）.py
+++ 백준/Silver/15650. N과 M （2）/N과 M （2）.py
@@ -3,6 +3,8 @@
 n, m = map(int, input().split())
 
 arr = [0] * (n + 1)
+
+# 현재 선택한 개수, 선택할 숫자의 시작점
 def DFS(num, cnt):
     if num == m:
         for i in range(m):
@@ -10,10 +12,16 @@ def DFS(num, cnt):
         print()
         return
 
-    for i in range(cnt, n+1):
+    # cnt보다 큰 숫자부터 해야하므로 매개변수가 2
+    # 이전보다 큰 숫자만 선택 (cnt부터 n까지 탐색)
+    for i in range(cnt, n + 1):
         arr[num] = i
         DFS(num + 1, i + 1)
-        # 어차피 같은 수가 아닌 증가하는 수열로 하기에 check를 해줄 필요가 없다.
+        # 현재 위치에 숫자를 저장하고, 다음 숫자를 선택한다.
 
 DFS(0, 1)
+# 0개 선택된 상태에서, 숫자 1부터 선택 시작한다.
 
+# 순열 : 순서가 중요, 모든 숫자를 한번씩 선택 DFS(num)
+# 조합 : 순서가 중요하지 않음, 오름차순 유지 필요 DFS(num, cnt)
+# 이전보다 더 큰 숫자만 선택해야 하므로 cnt를 활용해서 탐색 시작점을 제한해야 한다.
