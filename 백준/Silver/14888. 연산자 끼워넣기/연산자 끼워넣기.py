c5c31ef1b16e1fa33a39151a7fc73cc002b5d27e
Sangwoo Jin <61958748+Jin-s-work@users.noreply.github.com>
2025-02-26 14:21:41 +0900

    [Silver I] Title: 연산자 끼워넣기, Time: 176 ms, Memory: 112112 KB -BaekjoonHub

--- 백준/Silver/14888. 연산자 끼워넣기/연산자 끼워넣기.py
+++ 백준/Silver/14888. 연산자 끼워넣기/연산자 끼워넣기.py
@@ -1,33 +1,33 @@
 
-
+import sys
 n = int(input())
 arr = list(map(int, input().split()))
-a, b, c, d = list(map(int, input().split()))
-# + - * /
 
-Max = -float('inf')
-Min = float('inf')
+plus, minus, mult, div = map(int, input().split())
+
+Max = -sys.maxsize
+Min = sys.maxsize
 
 def DFS(num, idx, plus, minus, mult, div):
     global Max, Min
-    if idx == n - 1:
+    if idx == n:
         Max = max(Max, num)
         Min = min(Min, num)
         return
 
     if plus > 0:
-        DFS(num + arr[idx + 1] , idx + 1, plus - 1, minus, mult, div)
+        DFS(num + arr[idx], idx + 1, plus - 1, minus, mult, div)
     if minus > 0:
-        DFS(num - arr[idx + 1] , idx + 1, plus, minus - 1, mult, div)
+        DFS(num - arr[idx], idx + 1, plus, minus - 1, mult, div)
     if mult > 0:
-        DFS(num * arr[idx + 1] , idx + 1, plus, minus, mult - 1, div)
+        DFS(num * arr[idx], idx + 1, plus, minus, mult - 1, div)
     if div > 0:
         if num < 0:
-            DFS(-(abs(num) // arr[idx + 1]), idx + 1, plus, minus, mult, div - 1)
+            DFS(-((-num) // arr[idx]), idx + 1, plus, minus, mult, div - 1)
         else:
-            DFS(num // arr[idx + 1] , idx + 1, plus, minus, mult, div - 1)
+            DFS(num // arr[idx], idx + 1, plus, minus, mult, div - 1)
 
-DFS(arr[0], 0, a, b, c, d)
-
+DFS(arr[0], 1, plus, minus, mult, div)
 print(Max)
 print(Min)
+
