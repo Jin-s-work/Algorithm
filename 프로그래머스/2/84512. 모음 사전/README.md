0b070d0d249781b37acb8a368ffa789104b2617e
Sangwoo Jin <61958748+Jin-s-work@users.noreply.github.com>
2025-02-26 15:10:18 +0900

    [level 2] Title: 모음 사전, Time: 2.67 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/2/84512. 모음 사전/README.md
+++ 프로그래머스/2/84512. 모음 사전/README.md
@@ -1,10 +1,10 @@
 # [level 2] 모음 사전 - 84512 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/84512) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/84512?language=python3) 
 
 ### 성능 요약
 
-메모리: 10.6 MB, 시간: 1.46 ms
+메모리: 10.3 MB, 시간: 2.67 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 05월 03일 01:33:33
+2025년 02월 26일 15:10:15
 
 ### 문제 설명
 
--- 프로그래머스/2/84512. 모음 사전/모음 사전.py
+++ 프로그래머스/2/84512. 모음 사전/모음 사전.py
@@ -1,22 +1,18 @@
 def solution(word):
     answer = 0
     
+    alp = "AEIOU"
     arr = []
-    words = "AEIOU"
     
     def DFS(num, k):
         if num == 5:
             return
         
-        for i in range(len(words)):
-            arr.append(k + words[i])
-            DFS(num + 1, k + words[i])
-        # 처음에 A 넣고, AA, AAA, AAAA, AAAAA
-        # AAAAA에서 돌아와서 그 다음인 E로 넣는다. 따라서 AAAAE, 5개이므로 끝나니까 다음인 I
-        # AAAAI,,,, AAAAO, AAAAU, 다음은 AAAE
-    
+        for i in range(len(alp)):
+            arr.append(k + alp[i])
+            DFS(num + 1, k + alp[i])
+
     DFS(0, "")
     answer = arr.index(word) + 1
-    
     
     return answer
