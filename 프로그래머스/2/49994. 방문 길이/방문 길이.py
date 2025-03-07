e39f838f892415f98f6be014e01540fd59fb3284
Sangwoo Jin <61958748+Jin-s-work@users.noreply.github.com>
2025-03-08 01:06:59 +0900

    [level 2] Title: 방문 길이, Time: 0.40 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/2/49994. 방문 길이/방문 길이.py
+++ 프로그래머스/2/49994. 방문 길이/방문 길이.py
@@ -1,7 +1,9 @@
 def solution(dirs):
     answer = 0
     
-    arr = {'U' : (0,1), 'D' : (0,-1), 'R' : (1,0), 'L' : (-1, 0)}
+    arr = {'U' : (0, 1), 'D' : (0, -1), 'R' : (1, 0), 'L' : (-1, 0)}
+    
+
     s = set()
     
     x, y = 0, 0
@@ -10,10 +12,13 @@ def solution(dirs):
         ny = y + arr[k][1]
         
         if -5<=nx<=5 and -5<=ny<=5:
-            s.add((nx, ny, x, y))
             s.add((x, y, nx, ny))
+            s.add((nx, ny, x, y))
             
             x = nx
             y = ny
     
     return len(s) // 2
+        
+    
+    
