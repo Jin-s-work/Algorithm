011766c03cf60abeadc23fe49e90ea833e765ebb
Sangwoo Jin <61958748+Jin-s-work@users.noreply.github.com>
2025-03-21 15:21:49 +0900

    [Silver II] Title: 연결 요소의 개수, Time: 412 ms, Memory: 176384 KB -BaekjoonHub

--- 백준/Silver/11724. 연결 요소의 개수/연결 요소의 개수.py
+++ 백준/Silver/11724. 연결 요소의 개수/연결 요소의 개수.py
@@ -1,33 +1,27 @@
-import sys
-sys.setrecursionlimit(10**6)
-input = sys.stdin.readline
 
-
-from collections import deque
-
-
-def DFS(start):
-    check[start] = True
-    for i in arr[start]:
-        if not check[i]:
-            DFS(i)
-    # arr[v]를 i로 하고 arr[v]가 아직 false일 때 DFS로 들어간다.
 
 n, m = map(int, input().split())
-arr = [[] for _ in range(n+1)]
-check = [0 for _ in range(n+1)]
 
+arr = [[] for _ in range(n + 1)]
 for _ in range(m):
     a, b = map(int, input().split())
     arr[a].append(b)
     arr[b].append(a)
-    
-# check가 False인 곳에서 부터 시작해서
-# DFS를 해줘서 들어가고 나올 때 1씩 더해주면 개수를 구할 수 있다.
+
+check = [0] * (n + 1)
 ans = 0
-for i in range(1, n+1):
+
+def DFS(num):
+    check[num] = True
+    for next in arr[num]:
+        if not check[next]:
+            DFS(next)
+    # num을 방문표시해주고, 다음으로 넘어가는게 아직 False일 경우 들어간다.
+
+for i in range(1, n + 1):
     if not check[i]:
         DFS(i)
         ans += 1
+    # 아직 체크되지 않았으면 DFS해주고, ans 늘려준다.
 
 print(ans)
