8a40f56d5f674374dca996422c2c03a2b3d50f03
Sangwoo Jin <61958748+Jin-s-work@users.noreply.github.com>
2025-04-23 18:37:54 +0900

    [Gold V] Title: 강의실 배정, Time: 388 ms, Memory: 131464 KB -BaekjoonHub

--- 백준/Gold/11000. 강의실 배정/강의실 배정.py
+++ 백준/Gold/11000. 강의실 배정/강의실 배정.py
@@ -1,30 +1,22 @@
-import sys
-input = sys.stdin.readline
 
 import heapq
-
 n = int(input())
-   
+
 arr = []
 for _ in range(n):
     a, b = map(int, input().split())
-    arr.append([a, b])
-
+    arr.append((a, b))
 arr.sort()
 
-rm = []
-heapq.heappush(rm, arr[0][1])
-# 일단 먼저 처음 arr의 끝나는 시간을 넣는다.
+heap = []
+for start, end in arr:
+    if heap and heap[0] <= start:
+        heapq.heappop(heap)
+    heapq.heappush(heap, end)
 
-for i in range(1, n):
-    if arr[i][0] < rm[0]:
-        heapq.heappush(rm, arr[i][1])
-    # 끝나는 시간 보다 안에 있을 경우 새로 회의실 개설한다.
-    else: # 새로운 회의 개최 가능
-        heapq.heappop(rm)
-        # 새로운 회의로 시간 변경을 위해 pop 해준 뒤에 새 시간을 push한다.
-        heapq.heappush(rm, arr[i][1])
+print(len(heap))
 
-print(len(rm))
-
-            
+# 우선 순위 큐를 사용해서 가장 빨리 끝나는 수업시간을 계속 관리하는 것이다.
+# 우선 순위큐를 사용하면 O(1)에 꺼낼 수 있고, O(logn)에 업데이트가 가능하다.
+# 사용하는 이유 : 가장 빨리 끝나는 수업보다 늦게 시작하면 그 강의실을 재사용이 가능
+# 우선 순위큐가 아니면 전체확인해야 하는데 이는 O(n^2)
