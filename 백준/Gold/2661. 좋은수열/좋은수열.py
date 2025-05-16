8623676e68af2f4485cacbe03c41e6f62fcdd726
Sangwoo Jin <61958748+Jin-s-work@users.noreply.github.com>
2025-05-16 15:45:51 +0900

    [Gold IV] Title: 좋은수열, Time: 92 ms, Memory: 109544 KB -BaekjoonHub

--- 백준/Gold/2661. 좋은수열/좋은수열.py
+++ 백준/Gold/2661. 좋은수열/좋은수열.py
@@ -1,29 +1,25 @@
 
 
-
 n = int(input())
 
-def bad(st):
-    for i in range(1, len(st) // 2 + 1):
-        if st[-i:] == st[-2 * i : -i]:
-            return True
-    return False
-# 끝에서부터 i길이의 두 개의 부분 수열이 같으면 나쁜 수열이다.
-# st[-i:] : 수열의 마지막에서부터 길이 i만큼의 부분 수열
-# st[-2*i:-i] : 수열에서 그 이전의 i만큼의 부분 수열
+# 길이가 인접한 두개의 부분 수열
 
+def is_good(word):
+    for i in range(1, len(word) // 2 + 1):
+        # 마지막 i개와 그 앞의 i개를 비교
+        # for문을 통해서 i를 늘려준다.
+        if word[-i:] == word[-2 * i : -i]:
+            return False
+    return True
 
 def DFS(st):
     if len(st) == n:
         print(st)
         exit()
-
-    for next in '123':
-        if not bad(st + next):
-            DFS(st + next)
-    # 여기에서 123을 차례대로 붙여가면서 재귀적으로 넣어준다.
-    # 붙여주는 과정에서 나쁜 수열이 되는 경우에는 뒤에는 볼 필요가 없기에 여기에서 걸러준다.
-# 순서대로 차례대로 붙여주는 과정에서 제일 먼저 나온 것이 사전순으로 제일 먼저 이기에 바로 출력하고 끝낸다.
+    # 1, 2, 3
+    # 숫자를 먼저 탐색하고 나쁜 수열이 아닐 때 들어가주는 것이 훨씬 복잡도를 줄일 수 있다.
+    for i in range(1, 4):
+        if is_good(st + str(i)):
+            DFS(st + str(i))
 
 DFS("")
-
