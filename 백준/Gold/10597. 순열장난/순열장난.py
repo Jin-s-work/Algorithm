5caffaca5c17f7f4288824c7ffabdf2176e9ff48
Sangwoo Jin <61958748+Jin-s-work@users.noreply.github.com>
2025-05-16 15:17:49 +0900

    [Gold V] Title: 순열장난, Time: 220 ms, Memory: 112676 KB -BaekjoonHub

--- 백준/Gold/10597. 순열장난/순열장난.py
+++ 백준/Gold/10597. 순열장난/순열장난.py
@@ -2,7 +2,6 @@
 
 st = input()
 
-num = 0
 if len(st) >= 10:
     num = 9 + (len(st) - 9) // 2
 else:
@@ -11,33 +10,37 @@ else:
 check = [0] * (num + 1)
 flag = False
 
-def DFS(idx, ans):
+def DFS(idx, path):
     global flag
     if flag:
         return
-
+    # 한번 출력했으면 리턴해서 나온다!
 
     if idx == len(st):
-        if len(ans) == num:
-            print(" ".join(map(str, ans)))
+        if len(path) == num:
+            print(" ".join(map(str, path)))
             flag = True
         return
-    # 처음에 나온 수를 바로 출력하므로 flag를 통해 다음 거는 안나오게 한다.
 
-    # 일단 0이면 안되고, 숫자 자체도 제일 큰 수인 num보다 작아야 한다.
-    # 또한 check를 통해 지나간 적 없는 숫자여야 한다.
-    if int(st[idx:idx+1]) <= num and not check[int(st[idx:idx + 1])] and st[idx] != '0':
-        check[int(st[idx:idx + 1])] = True
-        DFS(idx + 1, ans + [st[idx:idx+1]])
-        check[int(st[idx:idx + 1])] = False
+    # 1자리 숫자 구하기
+    if idx < len(st):
+        one = int(st[idx])
+        # 한자리 수의 경우 앞 자리수가 0인거는 상관없고 그 자체만 0이 아니면 된다.
+        if one != 0 and not check[one]:
+            check[one] = True
+            DFS(idx + 1, path + [one])
+            check[one] = False
 
-    # 2자리수의 DFS를 진행할 때에는 2칸 뒤가 st를 넘으면 안되고 앞자리가 0이어도 안된다.
-    # 그리고 똑같이 num보다 작은 경우와 check를 통해 지나간 적 없는지 확
-    if idx + 1 < len(st) and st[idx] != '0':
-        if int(st[idx:idx + 2]) <= num and not check[int(st[idx:idx + 2])]:
-            check[int(st[idx:idx + 2])] = True
-            DFS(idx + 2, ans + [st[idx:idx+2]])
-            check[int(st[idx:idx + 2])] = False
+    # 2자리 숫자 구하기
+    if idx+ 1 < len(st):
+        two = int(st[idx : idx + 2])
+        if st[idx] != '0' and two <= num and not check[two]:
+            check[two] = True
+            DFS(idx + 2, path + [two])
+            check[two] = False
 
 DFS(0, [])
+
+
+
 
