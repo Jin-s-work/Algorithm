3e10cbe30f84f9065637f435e5e265deaa5cbd80
Sangwoo Jin <61958748+Jin-s-work@users.noreply.github.com>
2025-06-02 17:52:11 +0900

    [Gold IV] Title: 미친 로봇, Time: 392 ms, Memory: 114128 KB -BaekjoonHub

--- 백준/Gold/1405. 미친 로봇/미친 로봇.py
+++ 백준/Gold/1405. 미친 로봇/미친 로봇.py
@@ -1,45 +1,34 @@
 
 
+n, east, west, south, north = map(int, input().split())
 
+# 같은 곳을 한번보다 많이 이동하지 않을 때
 
-arr = list(map(int, input().split()))
-n = arr[0]
-east = arr[1]
-west = arr[2]
-south = arr[3]
-north = arr[4]
+move = [east / 100, west / 100, south / 100, north / 100]
+check = [[0] * (2 * n + 1) for _ in range(2 * n + 1)]
+dx = [0,0,1,-1]
+dy = [1,-1,0,0]
+total = 0
 
-check = [[0] * 29 for _ in range(29)]
-# 음수가 가능하므로 이렇게 하고 (14, 14)에서 시작해준다.
+def DFS(x, y, cnt, prob):
+    global total
 
-def DFS(num, p, x, y):
-    if num == n:
-        return p
+    if cnt == n:
+        total += prob
+        return
 
-    # 처음에 이런 풀이를 생각했는데 왜 안해봤을까
-    ans = 0
-    if check[x + 1][y] == 0:
-        check[x+1][y] = 1
-        ans += DFS(num + 1, p * east / 100, x + 1, y)
-        check[x+1][y] = 0
-    # 가고자 하는 방향이 아직 false일 경우 DFS로 가준다.
-    if check[x - 1][y] == 0:
-        check[x-1][y] = 1
-        ans += DFS(num + 1, p * west / 100, x - 1, y)
-        check[x-1][y] = 0
+    for i in range(4):
+        nx = x + dx[i]
+        ny = y + dy[i]
 
-    if check[x][y-1] == 0:
-        check[x][y-1] = 1
-        ans += DFS(num + 1, p * south / 100, x, y - 1)
-        check[x][y-1] = 0
+        if not check[nx][ny]:
+            check[nx][ny] = True
+            DFS(nx, ny, cnt + 1, prob * move[i])
+            check[nx][ny] = False
 
-    if check[x][y+1] == 0:
-        check[x][y+1] = 1
-        ans += DFS(num + 1, p * north / 100, x, y + 1)
-        check[x][y+1] = 0
+check[n][n] = True
+DFS(n, n, 0, 1)
+# 시작점을 중앙으로 해서 양수 좌표를 유지한다.
 
-    return ans
-
-check[14][14] = 1
-print(DFS(0, 1, 14, 14))
+print(f"{total:.9f}")
 
