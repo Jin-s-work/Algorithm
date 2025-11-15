65d9efb4975fe7f569bfdf01f54a469248814ce6
Sangwoo Jin <61958748+Jin-s-work@users.noreply.github.com>
2025-11-15 16:57:47 +0900

    [Gold III] Title: 괄호 추가하기, Time: 88 ms, Memory: 108384 KB -BaekjoonHub

--- 백준/Gold/16637. 괄호 추가하기/괄호 추가하기.py
+++ 백준/Gold/16637. 괄호 추가하기/괄호 추가하기.py
@@ -1,41 +1,47 @@
 
-
-n = int(input())
-exp = input()
-
 import sys
-ans = -sys.maxsize
+n = int(input())
 
-def Cal(a, op, b):
+def cal(a, op, b):
     if op == '+':
         return a + b
-    elif op == '-':
+    if op == '-':
         return a - b
-    elif op == '*':
-        return a * b
+    return a * b
 
-def DFS(idx, num):
+exp = input()
+
+num = []
+op = []
+for idx, ch in enumerate(exp):
+    if idx % 2 == 0:
+        num.append(int(ch))
+    else:
+        op.append(ch)
+# 숫자와 연산자 따로 넣어준다.
+
+if not op:
+    print(num[0])
+    sys.exit(0)
+
+ans = -sys.maxsize
+
+def DFS(i, acc):
     global ans
-
-    if idx >= n-1:
-        ans = max(ans, num)
+    if i == len(op):
+        ans = max(ans, acc)
         return
-    # 모든 연산을 수행하면 최댓값 갱신
 
-    # 괄호없이 바로 계산하는 경우
-    next = Cal(num, exp[idx], int(exp[idx + 1]))
-    # 숫자에다가 현재 연산자와 그 다음 숫자로 계산한다.
-    DFS(idx + 2, next)
-    # 계산 후에 idx + 2로 이동해서 다음 연산을 수행한다.
+    # 괄호 없이 현재 연산을 적용
+    v1 = cal(acc, op[i], num[i + 1])
+    DFS(i + 1, v1)
 
-    # 괄호를 추가해서 먼저 계산하는 경우 먼저 연산을 수행한 후에 기존 값과 연산한다.
-    if idx + 2 < n:
-        tmp = Cal(int(exp[idx + 1]), exp[idx + 2], int(exp[idx + 3]))
-        # 괄호 계산 괄호가 있다고 치고 먼저 계산하는 것이다.
-        next = Cal(num, exp[idx], tmp)
-        # 그 결과를 기존 값과 다시 연산하는 것이다.
-        # 뒤의 괄호를 계산하고 -> 기존 값과 원래의 연산
-        DFS(idx + 4, next)
+    # 다음 한 연산을 괄호로 먼저 계산 한 후에 현재 연산에 붙이기
+    if i + 1 < len(op):
+        mid = cal(num[i + 1], op[i + 1], num[i + 2])
+        v2 = cal(acc, op[i], mid)
+        DFS(i + 2, v2)
+    # 다음 값이 괄호를 사용한다 가정하고 들어가
 
-DFS(1, int(exp[0]))
+DFS(0, num[0])
 print(ans)
